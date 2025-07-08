4e4016a823c498af6abe57245f559e45170e9bbe
Eric Skylar <eric@fishdad.com>
2025-07-08 14:44:07 -0600

    fixed for pipeline

--- src/app/components/navbar-components/MobileSidebar.tsx
+++ src/app/components/navbar-components/MobileSidebar.tsx
@@ -1,6 +1,5 @@
 'use client';
 import React from 'react';
-import ThemeToggleSwitch from './ThemeToggle';
 
 export default function MobileSidebar({ open, onClose }: { open: boolean; onClose: () => void }) {
   return (
